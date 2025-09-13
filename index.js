1a77a67e165af31f9eeb31f95d42cc0187dab3af
rehankapaya <rehanaziz284@gmail.com>
2025-09-13 14:55:48 -0700

    changes ui

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const app = express();
 
 // Middleware
 app.use(cors(
-    {origin: 'http://localhost:5173', credentials: true}
+    {origin: ['http://localhost:5173','http://localhost:5174'], credentials: true}
 )); 
 app.use(express.json());
 app.use(cookieParser());
--- src/config/nodemailerTransporter.js
+++ src/config/nodemailerTransporter.js
@@ -4,8 +4,8 @@ const transporter = nodemailer.createTransport({
     service: "gmail",
     secure: true,
     auth: {
-        user: "pathseeker999@gmail.com",  // apna Gmail
-        pass: "onwl pzct awpt ivcl"         // Gmail app password
+        user: process.env.GMAIL_USER,  // apna Gmail
+        pass: process.env.GMAIL_APP_PASSWORD         // Gmail app password
     },
 });
 
